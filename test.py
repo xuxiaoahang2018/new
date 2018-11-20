28084167e2681aab851658649e228e2219f79313
xuxiaohang <2854873880@qq.com>
2018-11-20 12:09:29 +0800

    first commit

--- test.py
+++ test.py
@@ -1,10 +1,9 @@
-#! /home/xuxiaohang/.pyenv/shims/python
 from flask import Flask
 app = Flask(__name__)
 
-@app.route('/')
+@app.route("/")
 def index():
-    return "welcome index"
+    return "welcome"
 
 if __name__ == "__main__":
-    app.run(host="127.0.0.1",port="8080")
+    app.run(debug=True)
